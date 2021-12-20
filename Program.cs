25f9308bf01b1e7df2e315dae477beb1ba0e8137
antjamespadgett <padgettanthonyj@gmail.com>
2021-12-20 14:40:53 -0600

    completed project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EncapsulationProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var anthonysAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            anthonysAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank You!! Your balance is now {anthonysAccount.GetBalance()}");
+
+           
+        }
+    }
+}
